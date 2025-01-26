5252de0be462b505f9d90ed91bb88252e1f8513f
dsladeck <hertores@wp.pl>
2025-01-26 15:52:59 +0100

    test

---
+++ Assets/Scripts/PlayerControllerMovie.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerControllerMovie : MonoBehaviour
+{
+    public GameObject p3;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Debug.Log("Collision detected");
+            p3.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
